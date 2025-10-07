7ff8ca0472abe91a40c5edec09d4fb2827d69fa4
Ayan Ali <149358183+ayanali31990@users.noreply.github.com>
2025-10-07 11:45:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function removeColor() {
+  const select = document.getElementById("colorSelect");
+  // get selected index
+  const selectedIndex = select.selectedIndex;
+
+  // if a valid option is selected
+  if (selectedIndex !== -1) {
+    select.remove(selectedIndex);
+  }
+}
